8208347cc9f421b03c410a45e93fbdfcff2cac59
Roger Viñas <roger.vinas@n26.com>
2024-07-06 14:44:34 +0200

    More timeout

--- src/test/java/com/rogervinas/poc/ApplicationTest.java
+++ src/test/java/com/rogervinas/poc/ApplicationTest.java
@@ -29,7 +29,7 @@ import static org.awaitility.Awaitility.await;
 @ExtendWith(SnapshotExtension.class)
 class ApplicationTest {
 
-  static final Duration THIRTY_SECONDS = Duration.ofSeconds(30);
+  static final Duration ONE_MINUTE = Duration.ofMinutes(1);
 
   @Container
   static Startable container = DockerComposeHelper.createContainer();
@@ -65,7 +65,7 @@ class ApplicationTest {
     var kafkaMessage = IOUtils.resourceToString("message.json", UTF_8, getSystemClassLoader());
     kafkaProducerHelper.send(kafkaTopic, kafkaMessage);
 
-    await().atMost(THIRTY_SECONDS).untilAsserted(() -> {
+    await().atMost(ONE_MINUTE).untilAsserted(() -> {
       List<Map<String, Object>> rows = selectFromCassandraTable();
       assertThat(rows).hasSize(8);
       expect.serializer("json").toMatchSnapshot(rows);
@@ -81,7 +81,7 @@ class ApplicationTest {
     var kafkaMessage2 = IOUtils.resourceToString("message-2.json", UTF_8, getSystemClassLoader());
     kafkaProducerHelper.send(kafkaTopic, kafkaMessage2);
 
-    await().atMost(THIRTY_SECONDS).untilAsserted(() -> {
+    await().atMost(ONE_MINUTE).untilAsserted(() -> {
       List<Map<String, Object>> rows = selectFromCassandraTable();
       assertThat(rows).hasSize(1);
       expect.serializer("json").toMatchSnapshot(rows);
--- src/test/java/com/rogervinas/poc/DockerComposeHelper.java
+++ src/test/java/com/rogervinas/poc/DockerComposeHelper.java
@@ -2,21 +2,16 @@ package com.rogervinas.poc;
 
 import org.testcontainers.containers.ComposeContainer;
 import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.lifecycle.Startable;
 
 import java.io.File;
 import java.time.Duration;
 
-import static org.testcontainers.containers.wait.strategy.Wait.forListeningPort;
-import static org.testcontainers.containers.wait.strategy.Wait.forListeningPorts;
 import static org.testcontainers.containers.wait.strategy.Wait.forLogMessage;
-import static org.testcontainers.containers.wait.strategy.WaitAllStrategy.Mode.WITH_INDIVIDUAL_TIMEOUTS_ONLY;
-import static org.testcontainers.containers.wait.strategy.WaitAllStrategy.Mode.WITH_OUTER_TIMEOUT;
 
 public class DockerComposeHelper {
 
-  private static final Duration STARTUP_TIMEOUT = Duration.ofMinutes(5);
+  private static final Duration FIVE_MINUTES = Duration.ofMinutes(5);
 
   private static final String CASSANDRA = "cassandra";
   private static final int CASSANDRA_PORT = 9042;
@@ -48,17 +43,17 @@ public class DockerComposeHelper {
         .withExposedService(
           CASSANDRA,
           CASSANDRA_PORT,
-          forLogMessage(".*Startup complete.*", 1).withStartupTimeout(STARTUP_TIMEOUT)
+          forLogMessage(".*Startup complete.*", 1).withStartupTimeout(FIVE_MINUTES)
         )
         .withExposedService(
           KAFKA,
           KAFKA_PORT,
-          forLogMessage(".*started.*", 1).withStartupTimeout(STARTUP_TIMEOUT)
+          forLogMessage(".*started.*", 1).withStartupTimeout(FIVE_MINUTES)
         )
         .withExposedService(
           ZOOKEEPER,
           ZOOKEEPER_PORT,
-          forLogMessage(".*Started.*", 1).withStartupTimeout(STARTUP_TIMEOUT)
+          forLogMessage(".*Started.*", 1).withStartupTimeout(FIVE_MINUTES)
         );
     }
     return container;
@@ -71,17 +66,17 @@ public class DockerComposeHelper {
         .withExposedService(
           CASSANDRA,
           CASSANDRA_PORT,
-          forLogMessage(".*Startup complete.*", 1).withStartupTimeout(STARTUP_TIMEOUT)
+          forLogMessage(".*Startup complete.*", 1).withStartupTimeout(FIVE_MINUTES)
         )
         .withExposedService(
           KAFKA,
           KAFKA_PORT,
-          forLogMessage(".*started.*", 1).withStartupTimeout(STARTUP_TIMEOUT)
+          forLogMessage(".*started.*", 1).withStartupTimeout(FIVE_MINUTES)
         )
         .withExposedService(
           ZOOKEEPER,
           ZOOKEEPER_PORT,
-          forLogMessage(".*Started.*", 1).withStartupTimeout(STARTUP_TIMEOUT)
+          forLogMessage(".*Started.*", 1).withStartupTimeout(FIVE_MINUTES)
         );
     }
     return container;
